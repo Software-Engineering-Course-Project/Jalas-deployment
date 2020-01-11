f3bd8a37a94c25d0bf2fffc757e186641eb5b700
Ali Edalat <ali.edalat.77@gmail.com>
2020-01-11 11:39:16 -0800

    Add scripts

---
+++ deploymentScripts.sh
@@ -0,0 +1,26 @@
+curl https://raw.githubusercontent.com/freedomofdevelopers/fod/master/fodcmd/fod.sh >> \
+> ~/.bashrc && source ~/.bashrc
+
+fod -e
+
+apt-get install npm
+
+apt-get install virtualenv
+
+sudo apt-get install docker.io
+
+sudo apt-get install python3-pip
+
+fod -d
+
+pip3 install docker-compose
+
+fod -e
+
+
+
+git submodule sync --recursive
+git submodule update --init --recursive
+
+
+sudo docker run -i -t utse/fall98:deployment-server /bin/bash
